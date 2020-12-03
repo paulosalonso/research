e2f56d60cf861d063392d2a93619684401757a3a
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-12-03 16:23:51 -0300

    Initial commit

---
+++ src/main/java/com/github/paulosalonso/research/ResearchApplication.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/paulosalonso/research/ResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
