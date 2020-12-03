8566d0e104f9c49acf92cb8e12f8e73504291918
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-12-03 20:44:49 -0300

    Tests improvement

--- src/test/java/com/github/paulosalonso/research/usecase/research/ResearchReadTest.java
+++ src/test/java/com/github/paulosalonso/research/usecase/research/ResearchReadTest.java
@@ -1,5 +1,6 @@
 package com.github.paulosalonso.research.usecase.research;
 
+import com.github.paulosalonso.research.domain.Research;
 import com.github.paulosalonso.research.domain.ResearchCriteria;
 import com.github.paulosalonso.research.usecase.port.ResearchPort;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ResearchReadTest {
@@ -24,18 +28,26 @@ public class ResearchReadTest {
     @Test
     public void givenAUUIDWhenReadThenCallPort() {
         var id = UUID.randomUUID();
+        var toRead = Research.builder().build();
 
-        researchRead.read(id);
+        when(port.read(id)).thenReturn(toRead);
 
+        var result = researchRead.read(id);
+
+        assertThat(result).isSameAs(toRead);
         verify(port).read(id);
     }
 
     @Test
     public void givenAResearchCriteriaWhenSearchThenCallPort() {
         var criteria = ResearchCriteria.builder().build();
+        var toSearch = List.of(Research.builder().build());
 
-        researchRead.search(criteria);
+        when(port.search(criteria)).thenReturn(toSearch);
 
+        var result = researchRead.search(criteria);
+
+        assertThat(result).isSameAs(toSearch);
         verify(port).search(criteria);
     }
 }
