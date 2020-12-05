37247a5027fb0e1817563545dd0de41fc0a3917d
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-12-05 19:16:14 -0300

    Research mapper

--- src/main/java/com/github/paulosalonso/research/adapter/jpa/model/ResearchEntity.java
+++ src/main/java/com/github/paulosalonso/research/adapter/jpa/model/ResearchEntity.java
@@ -22,6 +22,8 @@ public class ResearchEntity {
     private String id;
 
     @NotBlank
+    private String title;
+
     private String description;
 
     @NotNull
---
+++ src/main/java/com/github/paulosalonso/research/adapter/mapper/ResearchMapper.java
@@ -0,0 +1,35 @@
+package com.github.paulosalonso.research.adapter.mapper;
+
+import com.github.paulosalonso.research.adapter.jpa.model.ResearchEntity;
+import com.github.paulosalonso.research.domain.Research;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class ResearchMapper {
+
+    public Research toDomain(ResearchEntity researchEntity) {
+        UUID id = researchEntity.getId() != null ? UUID.fromString(researchEntity.getId()) : null;
+
+        return Research.builder()
+                .id(id)
+                .title(researchEntity.getTitle())
+                .description(researchEntity.getDescription())
+                .startsOn(researchEntity.getStartsOn())
+                .endsOn(researchEntity.getEndsOn())
+                .build();
+    }
+
+    public ResearchEntity toEntity(Research research) {
+        String id = research.getId() != null ? research.getId().toString() : null;
+
+        return ResearchEntity.builder()
+                .id(id)
+                .title(research.getTitle())
+                .description(research.getDescription())
+                .startsOn(research.getStartsOn())
+                .endsOn(research.getEndsOn())
+                .build();
+    }
+}
