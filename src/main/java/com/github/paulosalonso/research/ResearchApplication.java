dfe4c7eb4212177f8c83ee543abc4c33dce88129
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-12-05 18:11:06 -0300

    UTC configuration

--- src/main/java/com/github/paulosalonso/research/ResearchApplication.java
+++ src/main/java/com/github/paulosalonso/research/ResearchApplication.java
@@ -3,10 +3,13 @@ package com.github.paulosalonso.research;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ResearchApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(ResearchApplication.class, args);
 	}
 
--- src/test/java/com/github/paulosalonso/research/usecase/research/ResearchCreateTest.java
+++ src/test/java/com/github/paulosalonso/research/usecase/research/ResearchCreateTest.java
@@ -9,7 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
@@ -27,7 +27,7 @@ public class ResearchCreateTest {
     public void givenAResearchWhenCreateThenCallPort() {
         var toSave = Research.builder()
                 .description("description")
-                .startsOn(LocalDateTime.now())
+                .startsOn(OffsetDateTime.now())
                 .build();
 
         researchCreate.create(toSave);
