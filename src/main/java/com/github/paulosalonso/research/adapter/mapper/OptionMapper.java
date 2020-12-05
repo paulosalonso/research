2609283cddf14faa87ea09a4e8d48f86a629f5a9
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-12-05 19:40:23 -0300

    Option mapper

---
+++ src/main/java/com/github/paulosalonso/research/adapter/mapper/OptionMapper.java
@@ -0,0 +1,29 @@
+package com.github.paulosalonso.research.adapter.mapper;
+
+import com.github.paulosalonso.research.adapter.jpa.model.OptionEntity;
+import com.github.paulosalonso.research.domain.Option;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class OptionMapper {
+
+    public Option toDomain(OptionEntity optionEntity) {
+        UUID id = optionEntity.getId() != null ? UUID.fromString(optionEntity.getId()) : null;
+
+        return Option.builder()
+                .id(id)
+                .description(optionEntity.getDescription())
+                .build();
+    }
+
+    public OptionEntity toEntity(Option option) {
+        String id = option.getId() != null ? option.getId().toString() : null;
+
+        return OptionEntity.builder()
+                .id(id)
+                .description(option.getDescription())
+                .build();
+    }
+}
---
+++ src/test/java/com/github/paulosalonso/research/adapter/mapper/OptionMapperTest.java
@@ -0,0 +1,65 @@
+package com.github.paulosalonso.research.adapter.mapper;
+
+import com.github.paulosalonso.research.adapter.jpa.model.OptionEntity;
+import com.github.paulosalonso.research.domain.Option;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OptionMapperTest {
+
+    private final OptionMapper mapper = new OptionMapper();
+
+    @Test
+    public void givenAnOptionWhenMapThenReturnEntity() {
+        var option = Option.builder()
+                .id(UUID.randomUUID())
+                .description("description")
+                .build();
+
+        var entity = mapper.toEntity(option);
+
+        assertThat(entity.getId()).isEqualTo(option.getId().toString());
+        assertThat(entity.getDescription()).isEqualTo(option.getDescription());
+    }
+
+    @Test
+    public void givenAnOptionWithoutIdWhenMapThenReturnEntity() {
+        var option = Option.builder()
+                .description("description")
+                .build();
+
+        var entity = mapper.toEntity(option);
+
+        assertThat(entity.getId()).isNull();
+        assertThat(entity.getDescription()).isEqualTo(option.getDescription());
+    }
+
+    @Test
+    public void givenAnOptionEntityWhenMapThenReturnDomain() {
+        var entity = OptionEntity.builder()
+                .id(UUID.randomUUID().toString())
+                .description("description")
+                .build();
+
+        var option = mapper.toDomain(entity);
+
+        assertThat(option.getId()).isEqualTo(UUID.fromString(entity.getId()));
+        assertThat(option.getDescription()).isEqualTo(entity.getDescription());
+    }
+
+    @Test
+    public void givenAnOptionEntityWithoutIdWhenMapThenReturnDomain() {
+        var entity = OptionEntity.builder()
+                .description("description")
+                .build();
+
+        var option = mapper.toDomain(entity);
+
+        assertThat(option.getId()).isNull();
+        assertThat(option.getDescription()).isEqualTo(entity.getDescription());
+    }
+
+}
