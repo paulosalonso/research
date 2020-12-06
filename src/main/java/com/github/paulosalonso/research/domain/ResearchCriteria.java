fba04ab4ea0c276551455c06cbfebb1cf3a61981
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-12-06 00:11:26 -0300

    Research gateway

--- src/main/java/com/github/paulosalonso/research/domain/ResearchCriteria.java
+++ src/main/java/com/github/paulosalonso/research/domain/ResearchCriteria.java
@@ -4,10 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.OffsetDateTime;
+
 @AllArgsConstructor
 @Getter
 @Builder
 public class ResearchCriteria {
 
+    private final String title;
     private final String description;
+    private final OffsetDateTime startsOnFrom;
+    private final OffsetDateTime startsOnTo;
+    private final OffsetDateTime endsOnFrom;
+    private final OffsetDateTime endsOnTo;
 }
