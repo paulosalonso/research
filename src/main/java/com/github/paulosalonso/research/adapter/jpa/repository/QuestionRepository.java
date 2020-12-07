5f50743224bcc08f5f5069fdbe0eb0654b5312df
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-12-07 18:29:37 -0300

    Question gateway

--- src/main/java/com/github/paulosalonso/research/adapter/jpa/repository/QuestionRepository.java
+++ src/main/java/com/github/paulosalonso/research/adapter/jpa/repository/QuestionRepository.java
@@ -1,14 +1,8 @@
 package com.github.paulosalonso.research.adapter.jpa.repository;
 
 import com.github.paulosalonso.research.adapter.jpa.model.QuestionEntity;
-import com.github.paulosalonso.research.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
-
-public interface QuestionRepository extends JpaRepository<QuestionEntity, String> {
-
-    List<Question> findByResearchId(String researchId);
-    List<Question> findByResearchIdAndId(String researchId, String questionId);
-
-}
+public interface QuestionRepository extends
+        JpaRepository<QuestionEntity, String>, JpaSpecificationExecutor<QuestionEntity> {}
