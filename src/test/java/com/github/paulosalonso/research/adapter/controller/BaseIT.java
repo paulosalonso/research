4bc45eaac7f5027fe4d9e04045af69a610386525
Paulo Alonso <paulo_alonso_@hotmail.com>
2021-01-05 15:32:06 -0300

    Run integrated tests over PostgreSQL with testcontainer

--- src/test/java/com/github/paulosalonso/research/adapter/controller/BaseIT.java
+++ src/test/java/com/github/paulosalonso/research/adapter/controller/BaseIT.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT, classes = ResearchApplication.class)
-@ActiveProfiles("h2")
 public class BaseIT {
 
     @Autowired
