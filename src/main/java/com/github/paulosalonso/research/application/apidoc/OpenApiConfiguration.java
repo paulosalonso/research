6058f3ae36f9eab1ecac922968a7dc4bd356cd56
Paulo Alonso <paulo_alonso_@hotmail.com>
2021-01-26 09:19:36 -0300

    Improves on OpenApiConfiguration

--- src/main/java/com/github/paulosalonso/research/application/apidoc/OpenApiConfiguration.java
+++ src/main/java/com/github/paulosalonso/research/application/apidoc/OpenApiConfiguration.java
@@ -8,28 +8,22 @@ import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.*;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
-import java.util.Scanner;
 
 import static java.util.function.Predicate.not;
 
 @Configuration
 @EnableWebMvc
-@EnableSwagger2
+@EnableOpenApi
 public class OpenApiConfiguration implements WebMvcConfigurer {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String nome = scanner.nextLine();
-    }
 
     @Bean
     public Docket docket() {
