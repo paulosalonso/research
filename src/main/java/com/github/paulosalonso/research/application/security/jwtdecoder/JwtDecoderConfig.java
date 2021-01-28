23eb3d7666a486d1ed3ea9ce280fba9817bafc51
Paulo Alonso <paulo_alonso_@hotmail.com>
2021-01-28 00:07:23 -0300

    Added log on JwtDecoder bean creation and fixed property name for jwk-set-uri.

--- src/main/java/com/github/paulosalonso/research/application/security/jwtdecoder/JwtDecoderConfig.java
+++ src/main/java/com/github/paulosalonso/research/application/security/jwtdecoder/JwtDecoderConfig.java
@@ -1,5 +1,6 @@
 package com.github.paulosalonso.research.application.security.jwtdecoder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import javax.crypto.spec.SecretKeySpec;
 
+@Slf4j
 @Profile("default")
 @Configuration
 public class JwtDecoderConfig {
@@ -18,6 +20,7 @@ public class JwtDecoderConfig {
             @Value("${security.jwt.signature.secret}") String secret,
             @Value("${security.jwt.signature.algorithm:HmacSHA256}") String algorithm) {
 
+        log.info("Decode JWT using secret key");
         SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(), algorithm);
         return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
